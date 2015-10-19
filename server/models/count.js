c524ca2927d6a982a795a6df010b2a5e8aa1b810
Peltoche <dev@halium.fr>
2015-10-19 14:41:00 +0200

    Change datatype name

--- server/models/count.js
+++ server/models/count.js
@@ -2,7 +2,7 @@
 
 var cozydb = require('cozydb');
 
-var CountModel = cozydb.getModel('Count', {
+var CountModel = cozydb.getModel('shared-count', {
   name:					String,
   description:	String,
 	allExpenses:	{type: Number, default: 0},
